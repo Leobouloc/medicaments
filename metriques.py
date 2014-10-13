0540a796560a185f145075d705b70fd6833ae1d8
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-10-13 23:06:26 +0200

    small  change in metrics

--- metriques.py
+++ metriques.py
@@ -6,31 +6,32 @@ Created on Mon Oct 13 10:14:27 2014
 """
 
 def plusieurs_labos_par_princeps(table_groupe):
-    table_groupe = table_groupe[table_groupe['Type'] == 0]
-    return len(set(table_groupe['LABO']))
-    
+    labos = table_groupe.loc[table_groupe['Type'] == 0, 'LABO']
+    return len(set(labos))
+
 
 def nombre_de_nouveaux_princeps(table_groupe, duree=12, niveau_atc = 4):
-    '''determine le nombre de princeps du même labo qui apparaissent dans les deux ans autour de la date de chute'''
+    '''determine le nombre de princeps du même labo qui apparaissent dans les
+        12 mois (duree) autour de la date de chute'''
     if niveau_atc == 4:
         string_atc = 'CODE_ATC_4'
     elif niveau_atc == 5 or niveau_atc == 6:
         string_atc = 'CODE_ATC'
-        
+
     classe = table_groupe[string_atc].iloc[0]
     groupe = table_groupe['Id_Groupe'].iloc[0]
-   
+
     # On prend  comme labo de référence celui qui commercialise le princeps en premier
     try:
-        index_princeps = table_groupe[table_groupe['Type'] == 0]['premiere_vente'].argmin()
-        labo = table_groupe[table_groupe['Type'] == 0].loc[index_princeps]['LABO']
+        index_princeps = table_groupe.loc[table_groupe['Type'] == 0, 'premiere_vente'].argmin()
+        labo = table_groupe[table_groupe['Type'] == 0].loc[index_princeps, 'LABO']
     except:
-        if len(table_groupe[table_groupe['Type'] == 0]) == 0:
+        if not any(table_groupe['Type'] == 0):
             labo = 'inconnu'
         else:
             print 'in else'
             labo = table_groupe[table_groupe['Type'] == 0].iloc[0]['LABO']
-    
+
     # On détermine la date de chute du brevet pour le groupe concerné
     date_chute = table_groupe.loc[~table_groupe['role'], 'premiere_vente'].min()
     if date_chute == 200301:
@@ -42,10 +43,10 @@ def nombre_de_nouveaux_princeps(table_groupe, duree=12, niveau_atc = 4):
         table = table[table[string_atc] == classe]
         table = table[table['Id_Groupe'] != groupe]
         selector1 = table.groupby('Id_Groupe')['premiere_vente'].apply(lambda x: x.argmin())
-        table = table.loc[selector1]        
+        table = table.loc[selector1]
         table = table[table['LABO'] == labo]
         table = table[table['Type'] == 0]
-        
+
         # On sélectionne les medicaments qui sont mis en vente autour de la chute du brevet
         def temporaire(x):
             return abs(period.index(x) - index_date_chute) <= duree and period.index(x) != 0
@@ -55,19 +56,19 @@ def nombre_de_nouveaux_princeps(table_groupe, duree=12, niveau_atc = 4):
 
     else:
         return (-1, 0)
-    
+
 
 def prix_moyen(table_groupe, average_over=12, prix='prix', selection='princeps'):
     '''Renvoie simplement le prix moyen des princeps (avant la chute), ou des génériques'''
     assert prix in ['prix_par_dosage', 'prix']
     assert selection in ['generiques', 'princeps']
-    
+
     if prix == 'prix_par_dosage':
         var_prix = period_prix_par_dosage
     elif prix == 'prix':
         var_prix = period_prix
-        
-    date_chute = table_groupe.loc[~table_groupe['role'],'premiere_vente'].min() 
+
+    date_chute = table_groupe.loc[~table_groupe['role'],'premiere_vente'].min()
 
     if not np.isnan(date_chute):
         if selection == 'princeps':
@@ -81,7 +82,7 @@ def prix_moyen(table_groupe, average_over=12, prix='prix', selection='princeps')
             return generique
     else:
         return np.nan
-    
+
 def volume_chute_brevet(table_groupe, average_over=12, span = 0):
     '''Calcule la variation relative de volume sur un an avant et après la chute de brevet pour le groupe'''
     date_chute = table_groupe.loc[~table_groupe['role'],'premiere_vente'].min()
@@ -89,7 +90,7 @@ def volume_chute_brevet(table_groupe, average_over=12, span = 0):
         chute_moins = period.index(date_chute) - average_over - span
         chute_plus = period.index(date_chute) + average_over + span
         if chute_moins >= 0 and chute_plus <= len(period):
-            somme_avant = table_groupe[period_dosage_rembourse].iloc[:, range(chute_moins, chute_moins + average_over)].sum().mean()  
+            somme_avant = table_groupe[period_dosage_rembourse].iloc[:, range(chute_moins, chute_moins + average_over)].sum().mean()
             somme_apres = table_groupe[period_dosage_rembourse].iloc[:, range(chute_plus - average_over, chute_plus)].sum().mean()
             variation = (somme_apres - somme_avant) / somme_avant
             return variation
@@ -105,14 +106,14 @@ def prix_chute_brevet(table_groupe, average_over=12, prix = 'prix_par_dosage', s
     '''selection = "ecart princeps_generique" : ecart de prix entre le princeps et les génériques mis sur le marché'''
     assert prix in ['prix_par_dosage', 'prix']
     assert selection in ['groupe', 'princeps', 'ecart_princeps_generique']
-    
-    date_chute = table_groupe.loc[~table_groupe['role'],'premiere_vente'].min()    
-    
+
+    date_chute = table_groupe.loc[~table_groupe['role'],'premiere_vente'].min()
+
     if prix == 'prix_par_dosage':
         var_prix = period_prix_par_dosage
     elif prix == 'prix':
         var_prix = period_prix
-        
+
     if selection == 'princeps':
         table_groupe = table_groupe[table_groupe['Type'] == 0]
 
@@ -130,7 +131,7 @@ def prix_chute_brevet(table_groupe, average_over=12, prix = 'prix_par_dosage', s
                 table_groupe_princeps = table_groupe[table_groupe['Type'] == 0]
                 table_groupe_generique = table_groupe[table_groupe['Type'] != 0]
                 # Renvoie l'écart relatif entre la moyenne un an avant la chute du princeps et le prix moyen des génériques lors de la chute
-                moyenne_princeps_avant = table_groupe_princeps[var_prix].iloc[:, range(chute_moins, chute_moins + average_over)].mean().mean()             
+                moyenne_princeps_avant = table_groupe_princeps[var_prix].iloc[:, range(chute_moins, chute_moins + average_over)].mean().mean()
                 generique = table_groupe_generique[var_prix].iloc[:, period.index(date_chute)].mean()
                 variation = (generique - moyenne_princeps_avant) / moyenne_princeps_avant
                 return variation
@@ -139,7 +140,7 @@ def prix_chute_brevet(table_groupe, average_over=12, prix = 'prix_par_dosage', s
         else:
             return np.nan
     else:
-        return np.nan        
+        return np.nan
 
 
 def labo_princeps(table_groupe):
@@ -152,23 +153,22 @@ def labo_princeps(table_groupe):
 def labo_to_int(serie):
 
     labos = list(set(base_brute['LABO']))
-    def function(x):  
+    def function(x):
         if isinstance(x, float):
             return np.nan
-        else: 
+        else:
             return labos.index(x)
     return serie.apply(lambda x: function(x))
-    
+
 #testo = base_brute.groupby('Id_Groupe').apply(lambda x: volume_chute_brevet(x, average_over = 12, span = 6))
 #testu = testo[testo.apply(lambda x: abs(x))<1]
 #plt.hist(list(testu[~testu.isnull()]), bins = 20)
 #plt.show()
 
-#nb_labos_par_princeps = base_brute.groupby('Id_Groupe').apply(lambda x: plusieurs_labos_par_princeps(x))   
-    
-'''Voir pour quels groupes la chute du brevet entraine un nouveau groupe dans la même classe par le même labo'''    
+#nb_labos_par_princeps = base_brute.groupby('Id_Groupe').apply(lambda x: plusieurs_labos_par_princeps(x))
+
+'''Voir pour quels groupes la chute du brevet entraine un nouveau groupe dans la même classe par le même labo'''
 nb_nouveaux_princeps = base_brute.groupby('Id_Groupe').apply(lambda x : nombre_de_nouveaux_princeps(x))
 nb_nouveaux_princeps = nb_nouveaux_princeps[nb_nouveaux_princeps != (-1,0)]
 nb_nouveaux_princeps = nb_nouveaux_princeps[nb_nouveaux_princeps.apply(lambda x: x[0] != 0)]
-
 
