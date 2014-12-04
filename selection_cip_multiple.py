a92478408fe5a0d59666305954130f99f8af9863
Leo <Leo@bouloc.eu>
2014-12-04 17:44:09 +0100

    petite modif

--- selection_cip_multiple.py
+++ selection_cip_multiple.py
@@ -15,23 +15,22 @@ def lambda_float(x):
         
 def sel_by_dosage_value(table):
     '''Sensé selectionner les CIP dont les dosages sont des nombres ronds '''
-   dosage = table['Dosage']
-   if dosage.notnull().all():
+    dosage = table['Dosage']
+    if dosage.notnull().all():
         dosage = dosage.str.split().apply(lambda x: x[0]).apply(lambda_float)
-        dosage = dosage[dosage.notnull()]
-        dosage = dosage[dosage != 0]
-        max_val = dosage.max()
-        min_val = dosage.min()
-        '''Renvoie True si tous les dosages sont définis et si l'écart relatif max au sein de la série est inférieur à 0,3'''
-        if (float(max_val) / float(min_val) < 1.3) & dosage.notnull().all() and ((dosage == 0).sum() == 0) :
-            n = dosage.apply(lambda x: round(math.log(x, 10)))
-            dosage = dosage / n.apply(lambda x: 10**x)
-            for i in range(3):
-                test = dosage == dosage.apply(round)
-                if (test.sum()) == 1:
-                    return test
-                else:
-                    dosage = 10 * dosage
-   return pd.Series(False, index = dosage.index)
+        if dosage.notnull().all() and (dosage != 0).all():
+            max_val = dosage.max()
+            min_val = dosage.min()
+            '''Renvoie True si tous les dosages sont définis et si l'écart relatif max au sein de la série est inférieur à 0,3'''
+            if (float(max_val) / float(min_val) < 1.3) and dosage.notnull().all() and (dosage != 0).all():
+                n = dosage.apply(lambda x: round(math.log(x, 10)))
+                dosage = dosage / n.apply(lambda x: 10**x)
+                for i in range(3):
+                    test = dosage == dosage.apply(round)
+                    if (test.sum()) == 1:
+                        return test
+                    else:
+                        dosage = 10 * dosage
+    return pd.Series(False, index = dosage.index)
             
     
