b951bc34caf8e2beb6f7a5f1d820fc3ddab59790
Leo <Leo@bouloc.eu>
2014-12-04 18:30:05 +0100

    change in selection

--- selection_cip_multiple.py
+++ selection_cip_multiple.py
@@ -5,8 +5,9 @@ Created on Thu Dec 04 10:37:16 2014
 @author: work
 """
    
-import math   
-   
+import math
+
+
 def lambda_float(x):
     try:
         return float(x)
@@ -17,20 +18,46 @@ def sel_by_dosage_value(table):
     '''Sensé selectionner les CIP dont les dosages sont des nombres ronds '''
     dosage = table['Dosage']
     if dosage.notnull().all():
+        # On prend la valeure numérique du dosage
+        
         dosage = dosage.str.split().apply(lambda x: x[0]).apply(lambda_float)
+        # On vérifie que tous les dosages sont bien définis et non nulls
         if dosage.notnull().all() and (dosage != 0).all():
             max_val = dosage.max()
             min_val = dosage.min()
-            '''Renvoie True si tous les dosages sont définis et si l'écart relatif max au sein de la série est inférieur à 0,3'''
-            if (float(max_val) / float(min_val) < 1.3) and dosage.notnull().all() and (dosage != 0).all():
-                n = dosage.apply(lambda x: round(math.log(x, 10)))
-                dosage = dosage / n.apply(lambda x: 10**x)
+            # On s'assure que l'écart relatif maximal est faible
+            if (float(max_val) / float(min_val)) < 1.3:
+                # On récupere la puissance de dix de chaque dosage en nottation scientifique
+                n = dosage.apply(lambda x: 10**(math.floor(math.log(x, 10))))
+                # On récupère la première partie de la notation scientifique
+                dosage = dosage / n
                 for i in range(3):
                     test = dosage == dosage.apply(round)
+                    # Si on a une unique ligne pour laquelle le dosage est nul on renvoie 
                     if (test.sum()) == 1:
                         return test
                     else:
                         dosage = 10 * dosage
     return pd.Series(False, index = dosage.index)
-            
     
+
+dos = dosage.str.split().str.get(0)
+dos = dos.apply(lambda_float)
+
+dose = dos[dos>0]
+power = dose.apply(lambda x: math.floor(math.log(x, 10)))
+
+table['power'] = range(len(table))
+for i in range(3):
+    cond = (10**i)*dose == (10**i)*dose.apply(round)
+    # Si on a une unique ligne pour laquelle le dosage est nul on renvoie 
+    table.loc[(cond) & table['power'] == 0, 'power'] = i
+        
+table['power'] = 0
+
+table.groupby('CIP')['power'].min()
+
+
+
+
+
