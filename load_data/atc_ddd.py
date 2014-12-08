fee9aabeb1dab6f0d8f78471f826366a1a269e2a
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2014-12-08 19:11:34 +0100

    minor

--- load_data/atc_ddd.py
+++ load_data/atc_ddd.py
@@ -42,12 +42,15 @@ def load_atc_ddd(info_utile, brut=False):
     # les problèmes viennent des NOTE. On a :
     #    assert tab.groupby(['CODE_ATC', 'MODE', 'UNITE', 'NOTE']).size().max() == 1
     # TODO: traiter "manuellement" ces cas
-    tab = tab.groupby(['CODE_ATC', 'MODE', 'UNITE']).mean().reset_index()
-    assert tab.groupby(['CODE_ATC', 'MODE', 'UNITE']).size().max() == 1
+    tab_out = tab.groupby(['CODE_ATC', 'MODE', 'UNITE'])['DDD'].mean().reset_index()
+    assert tab_out.groupby(['CODE_ATC', 'MODE', 'UNITE']).size().max() == 1
     # TODO: comprendre pourquoi, on a plusieurs unité
-    prob = tab.groupby(['CODE_ATC', 'MODE']).filter(lambda x: len(x) > 1)
-    assert tab.groupby(['CODE_ATC', 'MODE']).size().max() == 1
-    return tab
+    prob = tab_out.groupby(['CODE_ATC', 'MODE']).filter(lambda x: len(x) > 1)
+    assert tab_out.groupby(['CODE_ATC', 'MODE']).size().max() == 1
+    others_var = tab[['CODE_ATC', 'MODE', 'UNITE']  + [x for x in info_utile if
+                    x not in tab_out.columns]]
+    tab_out = tab_out.merge(others_var, on=['CODE_ATC', 'MODE', 'UNITE'])
+    return tab_out
 
 if __name__ == '__main__':
     info_utile = ['CODE_ATC', 'CHEMICAL_SUBSTANCE', 'DDD', 'UNITE', 'MODE']
@@ -56,5 +59,8 @@ if __name__ == '__main__':
     cond = test.CHEMICAL_SUBSTANCE.str.contains('statin')
     cond[cond.isnull()] = False
     test[cond]
+
+    test.groupby(['CODE_ATC', 'MODE', 'CHEMICAL_SUBSTANCE']).size().value_counts()
+    # => 19 groupes ont deux substances
     
     prob = test.CHEMICAL_SUBSTANCE.isnull()
